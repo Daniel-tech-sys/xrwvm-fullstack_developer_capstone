c4e3af7693362eac088b2b3d583ea7d554252586
Daniel-tech-sys <danielrafael9729@gmail.com>
2024-12-19 18:38:58 -0500

    Your descriptive commit message

--- server/database/app.js
+++ server/database/app.js
@@ -82,7 +82,7 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
     try {
-        const documents = await Dealerships.find();
+        const documents = await Dealerships.find({ id: req.params.id });
         res.json(documents);
       } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,6 @@
 # Uncomment the imports below before you add the function code
-# import requests
+
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -43,3 +44,12 @@ def analyze_review_sentiments(text):
 
 
 # Add code for posting review
+
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
